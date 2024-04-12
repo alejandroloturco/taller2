199d7b88e3a54269602a5c2f8602275032daa588
alejandroloturco <alejandro.loturco@udea.edu.co>
2024-04-12 10:49:13 -0500

    Punto 3

--- taller prequiz.py
+++ taller prequiz.py
@@ -3,4 +3,10 @@ import numpy as np
 matriz = np.random.randint(100, size=(40,30,100,10))
 
 new_matriz = matriz.copy()[:,:,:,0]
-print(new_matriz.size)
+
+print(f"Dimension: {new_matriz.ndim}")
+print(f"Tamaño: {new_matriz.size}")
+print(f"Forma: {new_matriz.shape}")
+print(f"Tipo: {new_matriz.dtype}")
+print(f"Tamaño en bytes de cada elemento en el array: {new_matriz.itemsize}")
+print(f"Tamaño de los bytes: {new_matriz.nbytes}")
