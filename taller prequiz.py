6683823a1a941873babc9ef430e68f7c96f3d470
alejandroloturco <alejandro.loturco@udea.edu.co>
2024-04-12 11:09:46 -0500

    punto 4

--- taller prequiz.py
+++ taller prequiz.py
@@ -10,3 +10,5 @@ print(f"Forma: {new_matriz.shape}")
 print(f"Tipo: {new_matriz.dtype}")
 print(f"Tamaño en bytes de cada elemento en el array: {new_matriz.itemsize}")
 print(f"Tamaño de los bytes: {new_matriz.nbytes}")
+
+new2_matriz = np.reshape(new_matriz,(new_matriz.shape[0],new_matriz.shape[1]*new_matriz.shape[2]))
